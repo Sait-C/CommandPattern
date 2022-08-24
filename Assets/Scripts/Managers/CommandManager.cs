8c2930c3953263eef9117281f84dce1343b0d4fa
Muharrem Sait ÇOKTAŞ <coktasmuharremsait@gmail.com>
2022-08-24 23:47:49 +0300

    Initial Commit

---
+++ Assets/Scripts/Managers/CommandManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CommandManager : MonoBehaviour
+{
+    private static CommandManager _instance;
+    public static CommandManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                Debug.LogError("The CommanManager is Null.");
+
+            return _instance;
+        }
+    }
+    private List<ICommand> _commandBuffer = new List<ICommand>();
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void AddCommand(ICommand command)
+    {
+        _commandBuffer.Add(command);
+    }
+
+    public void Rewind()
+    {
+        StartCoroutine(RewindRoutine());
+
+    }
+
+    IEnumerator RewindRoutine()
+    {
+        Debug.Log("Rewinding...");
+        foreach (var command in Enumerable.Reverse(_commandBuffer)) 
+        {
+            command.Undue();
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Finished..");
+    }
+
+    public void Play()
+    {
+        StartCoroutine(PlayRoutine());
+
+    }
+
+    IEnumerator PlayRoutine()
+    {
+        Debug.Log("Rewinding...");
+        foreach (var command in _commandBuffer)
+        {
+            command.Execute();
+            yield return new WaitForEndOfFrame();
+        }
+
+        Debug.Log("Finished..");
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    
+
+    ICommand moveUp, moveLeft, moveRight, moveDown;
+
+    [SerializeField]
+    private float _speed = 10.0f;
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveUp = new MoveUpCommand(this.transform, _speed);
+            moveUp.Execute();
+            CommandManager.Instance.AddCommand(moveUp);
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveDown = new MoveDownCommand(this.transform, _speed);
+            moveDown.Execute();
+            CommandManager.Instance.AddCommand(moveDown);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveLeft = new MoveLeftCommand(this.transform, _speed);
+            moveLeft.Execute();
+            CommandManager.Instance.AddCommand(moveLeft);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveRight = new MoveRightCommand(this.transform, _speed);
+            moveRight.Execute();
+            CommandManager.Instance.AddCommand(moveRight);
+        }
+    }
+
+   
+
+}
